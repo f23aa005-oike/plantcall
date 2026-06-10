8d1eb3c85f96a2363266308fb897a70c3b257ece
f23aa005-oike <f23aa005[chuo.ac.jp>
2026-06-10 10:55:35 +0900

    修正

--- src/calltest_vr5.py
+++ src/calltest_vr5.py
@@ -94,7 +94,7 @@ def start_basil_monitor():
                         send_to_spreadsheet(temp, hum, pres, lux, soil_per)
                         
                         # 1分待機（テスト用）
-                        time.sleep(180)
+                        time.sleep(1800)
                         
                 except ValueError:
                     pass
